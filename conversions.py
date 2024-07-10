36f2e88500e55e7d755395ac5d54857282a41892
Matt <matt2198@gmail.com>
2024-07-10 16:23:55 -0400

    unit conversion enhancements

--- conversions.py
+++ conversions.py
@@ -5,3 +5,24 @@ def celsius_to_fahrenheit(c):
 
 def fahrenheit_to_celsius(f):
     return (f - 32) * 5/9
+
+# Length conversions
+def meters_to_feet(m):
+    return m * 3.28084
+
+def kilometers_to_miles(km):
+    return km * 0.621371
+
+# Weight conversions
+def kilograms_to_pounds(kg):
+    return kg * 2.20462
+
+def grams_to_ounces(g):
+    return g * 0.035274
+
+# Volume conversions
+def liters_to_gallons(l):
+    return l * 0.264172
+
+def milliliters_to_ounces(ml):
+    return ml * 0.033814
